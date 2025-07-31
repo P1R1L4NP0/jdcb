7033b54552229f79178d5deec5e5ce647b95396b
P1R1L4NP0 <carloslanser1@gmail.com>
2025-07-31 20:24:08 -0300

    deu boa

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,18 @@
 import org.w3c.dom.ls.LSOutput;
 
+import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args){
         try{
-            DriverManager.getConnection("jdbc:mysql://localhost:3306", "root", "univille");
+            Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/projetopoo", "root", "univille");
+            Statement s = c.createStatement();
+            String Sql = "insert into PESSOAS(nome, cpf) " +
+                    "values('Tom', '09021984911')";
+            s.execute(Sql);
         }
         catch (SQLException e){
             System.out.println(e);
